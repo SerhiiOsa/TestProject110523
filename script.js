59850e0c83564a7e4f1f6c192a9a6bc7653e0516
Serhii Osa <oserdevit@gmail.com>
2023-05-16 13:38:58 +0300

    full adaptive version

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ const WELLCOME_BACK = document.getElementById('welcome-back');
 const CREATE_ACCOUNT = document.getElementById('create-account');
 const BLOCK_RIGHT = document.getElementById('block-right');
 const BLOCK_LEFT = document.getElementById('block-left');
-const SCREEN_WIDTH = window.innerWidth;
+let SCREEN_WIDTH = window.innerWidth;
 let position = null;
 let moveSize = null;
-WELLCOME_BACK.style.left = '0px';
-SIGN_IN.style.left = '0px';
 let moveElement = null;
 
 if(SCREEN_WIDTH >= 2000) {
@@ -18,6 +16,10 @@ if(SCREEN_WIDTH >= 2000) {
     BLOCK_RIGHT.style.left = '1536px';
     CREATE_ACCOUNT.style.left = '1536px';
     BLOCK_LEFT.style.left = '-768px';
+    SIGN_IN.style.top = '0px';
+    CREATE_ACCOUNT.style.top = '0px';
+    WELLCOME_BACK.style.left = '0px';
+    SIGN_IN.style.left = '0px';
 }
 
 if(SCREEN_WIDTH >= 1300 && SCREEN_WIDTH < 2000) {
@@ -27,6 +29,10 @@ if(SCREEN_WIDTH >= 1300 && SCREEN_WIDTH < 2000) {
     BLOCK_RIGHT.style.left = '1228px';
     CREATE_ACCOUNT.style.left = '1228px';
     BLOCK_LEFT.style.left = '-614px';
+    SIGN_IN.style.top = '0px';
+    CREATE_ACCOUNT.style.top = '0px';
+    WELLCOME_BACK.style.left = '0px';
+    SIGN_IN.style.left = '0px';
 }
 
 if(SCREEN_WIDTH >= 800 && SCREEN_WIDTH < 1300) {
@@ -36,7 +42,77 @@ if(SCREEN_WIDTH >= 800 && SCREEN_WIDTH < 1300) {
     BLOCK_RIGHT.style.left = '736px';
     CREATE_ACCOUNT.style.left = '736px';
     BLOCK_LEFT.style.left = '-368px';
+    SIGN_IN.style.top = '0px';
+    CREATE_ACCOUNT.style.top = '0px';
+    WELLCOME_BACK.style.left = '0px';
+    SIGN_IN.style.left = '0px';
 }
+
+if(SCREEN_WIDTH < 800) {
+    position = 0;
+    moveSize = 360;
+    HELLO_FRIEND.style.left = '0px';
+    WELLCOME_BACK.style.left = '-360px';
+    SIGN_IN.style.top = '280px';
+    CREATE_ACCOUNT.style.top = '280px';
+    BLOCK_RIGHT.style.left = '360px';
+    CREATE_ACCOUNT.style.left = '360px';
+    BLOCK_LEFT.style.left = '-360px';
+}
+
+window.addEventListener("resize", function() {
+    SCREEN_WIDTH = window.innerWidth;
+    if(SCREEN_WIDTH >= 2000) {
+        position = 768;
+        moveSize = 768;
+        HELLO_FRIEND.style.left = '768px';
+        BLOCK_RIGHT.style.left = '1536px';
+        CREATE_ACCOUNT.style.left = '1536px';
+        BLOCK_LEFT.style.left = '-768px';
+        SIGN_IN.style.top = '0px';
+        CREATE_ACCOUNT.style.top = '0px';
+        WELLCOME_BACK.style.left = '0px';
+        SIGN_IN.style.left = '0px';
+    }
+    
+    if(SCREEN_WIDTH >= 1300 && SCREEN_WIDTH < 2000) {
+        position = 614;
+        moveSize = 614;
+        HELLO_FRIEND.style.left = '614px';
+        BLOCK_RIGHT.style.left = '1228px';
+        CREATE_ACCOUNT.style.left = '1228px';
+        BLOCK_LEFT.style.left = '-614px';
+        SIGN_IN.style.top = '0px';
+        CREATE_ACCOUNT.style.top = '0px';
+        WELLCOME_BACK.style.left = '0px';
+        SIGN_IN.style.left = '0px';
+    }
+    
+    if(SCREEN_WIDTH >= 800 && SCREEN_WIDTH < 1300) {
+        position = 368;
+        moveSize = 368;
+        HELLO_FRIEND.style.left = '368px';
+        BLOCK_RIGHT.style.left = '736px';
+        CREATE_ACCOUNT.style.left = '736px';
+        BLOCK_LEFT.style.left = '-368px';
+        SIGN_IN.style.top = '0px';
+        CREATE_ACCOUNT.style.top = '0px';
+        WELLCOME_BACK.style.left = '0px';
+        SIGN_IN.style.left = '0px';
+    }
+    
+    if(SCREEN_WIDTH < 800) {
+        position = 0;
+        moveSize = 360;
+        HELLO_FRIEND.style.left = '0px';
+        WELLCOME_BACK.style.left = '-360px';
+        SIGN_IN.style.top = '280px';
+        CREATE_ACCOUNT.style.top = '280px';
+        BLOCK_RIGHT.style.left = '360px';
+        CREATE_ACCOUNT.style.left = '360px';
+        BLOCK_LEFT.style.left = '-360px';
+    }
+});
 
 function signUp() {
     if(SCREEN_WIDTH >= 800) {
@@ -51,7 +127,18 @@ function signUp() {
             }
         }, 1);
     } else{
-        return;
+        moveElement = setInterval(() => {
+            if(position >= moveSize) {
+                clearInterval(moveElement);
+                moveElement = null;
+            } else {
+                position +=2;
+                HELLO_FRIEND.style.left = position + 'px';
+                WELLCOME_BACK.style.left = position - moveSize + 'px';
+                CREATE_ACCOUNT.style.left = moveSize - position + 'px';
+                SIGN_IN.style.left = - position + 'px';
+            }
+        }, 1);
     }
 };
 
@@ -67,8 +154,19 @@ function signIn() {
                 SIGN_IN.style.left = position - moveSize + 'px';
             }
         }, 1);
-    } else {
-        return;
+    } else{
+        moveElement = setInterval(() => {
+            if(position <= 0) {
+                clearInterval(moveElement);
+                moveElement = null;
+            } else {
+                position -=2;
+                HELLO_FRIEND.style.left = position + 'px';
+                WELLCOME_BACK.style.left = position - moveSize + 'px';
+                CREATE_ACCOUNT.style.left = moveSize - position + 'px';
+                SIGN_IN.style.left = - position + 'px';
+            }
+        }, 1);
     }
 };
 
